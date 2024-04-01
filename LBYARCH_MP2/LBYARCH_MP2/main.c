aac7ba06c144db6f8ae1c7a37e1c2c5f9426d496
wailingwailord <maximusburce@gmail.com>
2024-04-01 16:36:26 +0800

    Initialized random float for each array

--- LBYARCH_MP2/LBYARCH_MP2/main.c
+++ LBYARCH_MP2/LBYARCH_MP2/main.c
@@ -3,8 +3,11 @@
 #include <windows.h>
 #include "cfunc.h"
 #include <time.h>
+#include <math.h>
 
-extern void asm_version(float A, float* X, float* Y, int n, float* Z);
+#define MAX_DECIMAL_PLACE 10
+
+extern void asm_version(float A, float* X, float* Y, int n, float* Z) {};
 
 int main() {
 	//TODO: 
@@ -14,10 +17,35 @@ int main() {
 
 	//////////////////////////////////////////////////
 	//FOR TESTING
-	int n = 5;
-	float A = 2.0;
-	float X[] = { 1.5, 2.0, 3.25, 4.0, 5.001 };
-	float Y[] = { 11.0, 12.5, 13.0, 14.34, 15.0 };
+	int n = 10;
+	float A = 2.5;
+	float* X = (float*)malloc(n * sizeof(float));
+	float* Y = (float*)malloc(n * sizeof(float));
+
+	srand(time(NULL));
+
+	for (int i = 0; i < n; i++) {
+		float random_float = (float)(rand() % (MAX_DECIMAL_PLACE * 10)) / 10.0f;
+		X[i] = random_float;
+	}
+
+	for (int i = 0; i < n; i++) {
+		float random_float = (float)(rand() % (MAX_DECIMAL_PLACE * 10)) / 10.0f;
+		Y[i] = random_float;
+	}
+
+	printf("Generated values for X:\n");
+	for (int i = 0; i < 10; i++) {
+		printf("%.1f ", X[i]);
+	}
+	printf("\n");
+
+	printf("Generated values for Y:\n");
+	for (int i = 0; i < 10; i++) {
+		printf("%.1f ", Y[i]);
+	}
+	printf("\n\n");
+
 	float* Z = (float*)malloc(n * sizeof(float));
 
 	clock_t start = clock();
@@ -32,6 +60,11 @@ int main() {
 	printf("Z[2] = %f\n", Z[2]);
 	printf("Z[3] = %f\n", Z[3]);
 	printf("Z[4] = %f\n", Z[4]);
+	printf("Z[5] = %f\n", Z[5]);
+	printf("Z[6] = %f\n", Z[6]);
+	printf("Z[7] = %f\n", Z[7]);
+	printf("Z[8] = %f\n", Z[8]);
+	printf("Z[9] = %f\n", Z[9]);
 	printf("Time measured: %.3f seconds.\n\n", elapsed);
 
 	start = clock();
@@ -46,6 +79,11 @@ int main() {
 	printf("Z[2] = %f\n", Z[2]);
 	printf("Z[3] = %f\n", Z[3]);
 	printf("Z[4] = %f\n", Z[4]);
+	printf("Z[5] = %f\n", Z[5]);
+	printf("Z[6] = %f\n", Z[6]);
+	printf("Z[7] = %f\n", Z[7]);
+	printf("Z[8] = %f\n", Z[8]);
+	printf("Z[9] = %f\n", Z[9]);
 	printf("Time measured: %.3f seconds.\n", elapsed);
 	//////////////////////////////////////////////////
 
